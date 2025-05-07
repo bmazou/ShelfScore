e949d09bbd3aaed5b662aad6b7ca6eda1f01e31a
bmazou <codprojmail@gmail.com>
2025-05-07 14:18:13 +0200

    Make foreign keys required properties

--- ShelfScore.Shared/Entities/Book.cs
+++ ShelfScore.Shared/Entities/Book.cs
@@ -4,12 +4,12 @@ public class Book
 {
     public int Id { get; set; }
     public required string Title { get; set; }
-    public DateOnly ReleaseDate { get; set; }    
+    public required DateOnly ReleaseDate { get; set; }    
     public int RatingsSum { get; set; }
     public int RatingsCount { get; set; }
     public float RatingsAverage { get; private set; }
 
-    public int AuthorId { get; set; }
+    public required int AuthorId { get; set; }
     public required Author Author { get; set; } 
     
     public ICollection<Rating> Ratings { get; set; } = [];
